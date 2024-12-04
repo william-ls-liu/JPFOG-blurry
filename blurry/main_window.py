9ed7a60c60f4aecc892d2baf72a9221c5d4c701e
William Liu <will123liu@gmail.com>
2024-12-04 11:25:24 -0800

    feat: add filename builder widgets

--- blurry/main_window.py
+++ blurry/main_window.py
@@ -1,16 +1,25 @@
 # Author: William Liu <liwi@ohsu.edu>
 
-from PySide6.QtCore import QStandardPaths, Qt, Slot
+from PySide6.QtCore import QStandardPaths, Qt, QUrl, Slot
 from PySide6.QtGui import QAction, QIcon
 from PySide6.QtMultimedia import QAudioOutput, QMediaPlayer
 from PySide6.QtMultimediaWidgets import QVideoWidget
 from PySide6.QtWidgets import (
     QApplication,
+    QComboBox,
     QDialog,
     QFileDialog,
+    QGridLayout,
+    QHBoxLayout,
+    QHeaderView,
     QLabel,
+    QLineEdit,
     QMainWindow,
+    QPushButton,
     QSlider,
+    QSpinBox,
+    QTableWidget,
+    QTableWidgetItem,
     QToolBar,
     QVBoxLayout,
     QWidget,
@@ -23,6 +32,7 @@ class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
 
+        # Widgets for video and audio playback
         self._media_player = QMediaPlayer()
         self._audio_output = QAudioOutput()
         self._video_widget = QVideoWidget()
@@ -61,12 +71,100 @@ class MainWindow(QMainWindow):
         # Label for video title
         self._video_label = QLabel("No file selected", parent=None)
 
+        # Filename builder widgets
+        self._side_id_label = QLabel("Site ID", parent=self)
+        self._site_id_combobox = QComboBox(parent=self)
+        self._site_id_combobox.addItems(
+            ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "Rochester"]
+        )
+
+        self._subject_id_label = QLabel("Subject ID", parent=self)
+        self._subject_id_spinbox = QSpinBox(parent=self)
+        self._subject_id_spinbox.setRange(0, 999)
+
+        self._freezer_status_label = QLabel("Freezer Status", parent=self)
+        self._freezer_status_combobox = QComboBox(parent=self)
+        self._freezer_status_combobox.addItems(["FR", "NF", "CO"])
+
+        self._session_id_label = QLabel("Session ID", parent=self)
+        self._session_id_combobox = QComboBox(parent=self)
+        self._session_id_combobox.addItems(["ses01", "ses02"])
+
+        self._medication_status_label = QLabel("On/Off", parent=self)
+        self._medication_status_combobox = QComboBox(parent=self)
+        self._medication_status_combobox.addItems(["On", "Off"])
+
+        self._trial_id_label = QLabel("Trial ID", parent=self)
+        self._trial_id_combobox = QComboBox(parent=self)
+        self._trial_id_combobox.addItems(
+            [
+                "stwalk",
+                "dtwalk",
+                "stcarr",
+                "stturn",
+                "dtturn",
+                "stshuf",
+                "stagil",
+                "stdoor",
+            ]
+        )
+
+        self._retry_label = QLabel("Retry (leave 0 if N/A)", parent=self)
+        self._retry_spinbox = QSpinBox(parent=self)
+        self._retry_spinbox.setRange(0, 999)
+
+        self._video_plane_label = QLabel("Video Plane", parent=self)
+        self._video_plane_combobox = QComboBox(parent=self)
+        self._video_plane_combobox.addItems(["front", "sagit"])
+
+        self._add_to_queue_button = QPushButton("Add to queue", parent=self)
+        self._add_to_queue_button.clicked.connect(self.enqueue)
+
+        filename_builder_layout = QGridLayout()
+        filename_builder_layout.addWidget(self._side_id_label, 0, 0)
+        filename_builder_layout.addWidget(self._site_id_combobox, 0, 1)
+        filename_builder_layout.addWidget(self._subject_id_label, 1, 0)
+        filename_builder_layout.addWidget(self._subject_id_spinbox, 1, 1)
+        filename_builder_layout.addWidget(self._freezer_status_label, 2, 0)
+        filename_builder_layout.addWidget(self._freezer_status_combobox, 2, 1)
+        filename_builder_layout.addWidget(self._session_id_label, 3, 0)
+        filename_builder_layout.addWidget(self._session_id_combobox, 3, 1)
+        filename_builder_layout.addWidget(self._medication_status_label, 4, 0)
+        filename_builder_layout.addWidget(self._medication_status_combobox, 4, 1)
+        filename_builder_layout.addWidget(self._trial_id_label, 5, 0)
+        filename_builder_layout.addWidget(self._trial_id_combobox, 5, 1)
+        filename_builder_layout.addWidget(self._retry_label, 6, 0)
+        filename_builder_layout.addWidget(self._retry_spinbox, 6, 1)
+        filename_builder_layout.addWidget(self._video_plane_label, 7, 0)
+        filename_builder_layout.addWidget(self._video_plane_combobox, 7, 1)
+        filename_builder_layout.addWidget(self._add_to_queue_button, 8, 0, 1, 2)
+
+        # File queue widget
+        self._queue = QTableWidget(parent=self)
+        self._queue.setRowCount(0)
+        self._queue.setColumnCount(2)
+        self._queue.setHorizontalHeaderLabels(["Original File", "New Name"])
+        horizontal_header = self._queue.horizontalHeader()
+        horizontal_header.setSectionResizeMode(
+            QHeaderView.Stretch
+        )  # make cols fill available space
+
+        # Layout for the video player
+        video_player_layout = QVBoxLayout()
+        video_player_layout.addWidget(self._video_label)
+        video_player_layout.addWidget(self._video_widget, stretch=1)
+        video_player_layout.addWidget(self._scrubber)
+
+        # Layout for the file queue and filename builder
+        queue_layout = QVBoxLayout()
+        queue_layout.addLayout(filename_builder_layout)
+        queue_layout.addWidget(self._queue)
+
+        central_layout = QHBoxLayout()
+        central_layout.addLayout(video_player_layout, stretch=3)
+        central_layout.addLayout(queue_layout)
         self._central_widget = QWidget(parent=self)
-        layout = QVBoxLayout()
-        layout.addWidget(self._video_label)
-        layout.addWidget(self._video_widget, stretch=1)
-        layout.addWidget(self._scrubber)
-        self._central_widget.setLayout(layout)
+        self._central_widget.setLayout(central_layout)
         self.setCentralWidget(self._central_widget)
 
     def closeEvent(self, event):
@@ -84,6 +182,10 @@ class MainWindow(QMainWindow):
             self._media_player.setSource(file_dialog.selectedUrls()[0])
             self._video_label.setText(file_dialog.selectedFiles()[0])
             self._media_player.play()
+            self._audio_output.setVolume(0)
+        else:
+            self._media_player.setSource(QUrl())
+            self._video_label.setText("No file selected")
 
     def _ensure_stopped(self):
         if self._media_player.playbackState() != QMediaPlayer.StoppedState:
@@ -114,6 +216,11 @@ class MainWindow(QMainWindow):
         current_position = self._scrubber.value()
         self._media_player.setPosition(current_position)
         self._media_player.play()
+
+    @Slot()
+    def enqueue(self) -> None:
+        print(self._media_player.source().toLocalFile())
+        print(type(self._media_player.source().toLocalFile()))
 
 
 if __name__ == "__main__":
